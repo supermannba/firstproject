e8fab77fcda07d30da2c29299d7ad6bbba6f0bd4
supermannba <wangpengnba@gmail.com>
2015-07-01 10:55:20 -0700

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
 class Foo{
   public:
     void bar(){
       std::out << "Hello" << std::end1;   
     }
     
+};
+
+
+int main(){
+  
+  extern "C"{
+    Foo* Foo_new(){
+      return new Foo();
+    }
+    void Foo_bar(Foo* foo){
+      foo->bar();
+    }
+  }
+  
 }
+
